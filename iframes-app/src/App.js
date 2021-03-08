38b010bd0154efedce1f63f0bac65c63c905612b
Andreslon <andreslon@outlook.com>
2021-03-08 10:26:00 -0500

    Added menu to change iframes

--- iframes-app/src/App.js
+++ iframes-app/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText' 
 import './App.css';
+const drawerWidth = 240
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    height: window.innerHeight,
+    width: '100%',
+  }
+}))
 
-function App() {
+const forms = [
+  { text: 'Hubspot', page: 'https://www.hubspot.com' },
+  { text: 'Monday', page: 'https://monday.com' }
+]
+
+const App = (props) => {
+
+  const [Url, setUrl] = useState('')
+
+  const redirectUrl = (selectedRoute) => {
+    const urlRedirect = `${selectedRoute}`
+    console.log('loading:', urlRedirect)
+    setUrl(urlRedirect)
+  }
+
+  const classes = useStyles()
+
+  const Select = (page) => () => {
+    redirectUrl(page)
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <List>
+          {forms.map((form) => (
+            <ListItem button key={form.text} onClick={Select(form.page)}>
+              <ListItemText primary={form.text} />
+            </ListItem>
+          ))}
+        </List>
+        
+      </Drawer>
+      <main className={classes.content}>
+        <iframe
+          title="mod4"
+          src={Url}
+          height="100%"
+          width="100%"
+          frameBorder="0"
+        ></iframe>
+      </main>
     </div>
   );
 }
 
-export default App;
+export default App
+ 
